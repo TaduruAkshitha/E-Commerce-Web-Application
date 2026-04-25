e562e763893d94c72a777066375fd58be9a056a3
Akshitha <160624733058@stanley.edu.in.com>
2026-04-25 17:23:08 +0530

    fix: image URLs, fonts, navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,31 @@
-import Profile from './pages/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Cart from './pages/Cart';
-import Orders from './pages/Orders';
 import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+import Profile from './pages/Profile';
 import Admin from './pages/Admin';
 import ProtectedRoute from './components/ProtectedRoute';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Toaster position="top-right" />
       <Routes>
-        <Route path="/"         element={<Home />} />
-        <Route path="/login"    element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/cart"     element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-        <Route path="/orders"   element={<ProtectedRoute><Orders /></ProtectedRoute>} />
+        <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
         <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-        <Route path="/admin"    element={<ProtectedRoute adminOnly><Admin /></ProtectedRoute>} />
+        <Route path="/orders" element={<ProtectedRoute><Orders /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
